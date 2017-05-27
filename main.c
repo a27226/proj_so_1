c233f8c522003176bd144583ac7921f88acda4d7
a27226 <nelson.canhoto@live.com.pt>
2017-05-27 01:00:09 +0100

    Finished vs cpu mode and color checking bug.

--- main.c
+++ main.c
@@ -19,7 +19,9 @@ void handler(int signum) //Sig handler
 {
 }
 
-void jogo();
+void jogocpu(int dif);
+
+void jogopvp(int dif);
 
 int main()
 {
@@ -28,7 +30,7 @@ int main()
     	printf("Mastermind - Game\n\n");
 	printf("Insira um numero de 1 a 5: ");
     	printf("\n\n1 - Jogar contra pc.\n");
-    	printf("2 - ...\n");
+    	printf("2 - Jogar contra outro jogador.\n");
     	printf("3 - ...\n");
     	printf("4 - ...\n");
     	printf("5 - Sair\n\n");
@@ -37,9 +39,10 @@ int main()
 	switch(x)
 	{
 		case 1 :
-		    	jogo();
+		    	jogocpu(4);
 			break;
 		case 2 :
+			jogopvp(4);
 			break;
 		case 3 :
 			break;
@@ -50,7 +53,7 @@ int main()
 	}
 }
 
-void jogo()
+void jogocpu(int dif)
 {
 	signal(SIGCONT, handler);
 	int p1[2],p2[2], codeclient[4],codeserver[4], colors[6]={0,0,0,0,0,0}, checkp[4]={0,0,0,0}, pid, i, j, k, clientpid, serverpid,rc=0,rp=0;
@@ -68,7 +71,7 @@ void jogo()
 			read(p2[0],&serverpid,sizeof(&serverpid));
 			kill(serverpid, SIGCONT);
 			pause();
-			for(int i=0;i<10;i++)//game cycle for client
+			for(int i=0;i<25;i++)//game cycle for client
 			{
 				for(j=0;j<4;j++)//code choosing
 				{	//escolhe codigo
@@ -116,6 +119,8 @@ void jogo()
 			}
 			printf(RED"Esgotaram-se as tentativas...\n\nGAME OVER\n\n");
 			sleep(1);
+			printf(COLORRESET"");
+			fflush(stdout);
 			kill(serverpid,SIGTERM);
 		}
 		else
@@ -129,8 +134,139 @@ void jogo()
 			srand(time(NULL));
 			for(int a=0;a<4;a++)//code choosing
 				codeserver[a]= rand()%6;
-			for(int c=0;c<4;c++)
-				printf("%d\n",codeserver[c]);
+			kill(clientpid,SIGCONT);
+			pause();//passa controlo para o cliente (kil e depois pause)
+			//server checks if code is right
+			for(int b=0;b<25;b++)
+			{	
+				rc=0;
+				rp=0;
+				fflush(stdout);
+				read(p1[0], codeclient, sizeof(codeclient));
+				for(k=0;k<4;k++)
+				{
+					for(int l=0;l<4;l++)
+					{
+						if(codeclient[k]==codeserver[l])
+						{
+							if(colors[codeclient[k]]==0)
+							{
+								rc++;
+								colors[codeclient[k]]=1;
+							}
+							if(l==k)
+							{
+								if(checkp[k]==0)
+								{
+									rp++;
+									checkp[k]=1;
+									break;
+								}
+							}
+						}
+					}	
+				}
+				colors[0]=0;
+				colors[1]=0;
+				colors[2]=0;
+				colors[3]=0;
+				colors[4]=0;
+				colors[5]=0;
+				checkp[0]=0;
+				checkp[1]=0;
+				checkp[2]=0;
+				checkp[3]=0;
+				write(p2[1],&rp,sizeof(&rp));
+				sleep(1);
+				write(p2[1],&rc,sizeof(&rc));
+				kill(clientpid,SIGCONT);
+				pause();//shift control
+			}
+		}
+	return ;
+}
+
+void jogopvp(int dif)
+{
+	signal(SIGCONT, handler);
+	int p1[2],p2[2], codeclient[4],codeserver[4], colors[6]={0,0,0,0,0,0}, checkp[4]={0,0,0,0}, pid, i, j, k, clientpid, serverpid,rc=0,rp=0;
+	pipe(p1);
+	pipe(p2);
+	pid = fork(); 
+	//while(1==1)
+		if (0==pid)
+		{
+			//cliente
+			int colorc, posc;
+			clientpid=getpid();
+			write(p1[1], &clientpid, sizeof(&clientpid));
+			sleep(1);
+			read(p2[0],&serverpid,sizeof(&serverpid));
+			kill(serverpid, SIGCONT);
+			pause();
+			for(int i=0;i<25;i++)//game cycle for client
+			{
+				for(j=0;j<4;j++)//code choosing
+				{	//escolhe codigo
+					do
+					{
+						printf(COLORRESET"Insira cor %d\n",(j+1));
+						fflush(stdout);
+						printf(RED"0 - Vermelho\n");
+						fflush(stdout);
+						printf(GREEN"1 - Verde\n");
+						fflush(stdout);
+						printf(YELLOW"2 - Amarelo\n");
+						fflush(stdout);
+						printf(BLUE"3 - Azul\n");
+						fflush(stdout);
+						printf(MAGENTA"4 - Magenta\n");
+						fflush(stdout);
+						printf(CYAN"5 - Ciano\n");
+						fflush(stdout);
+						printf(COLORRESET"");
+						fflush(stdout);
+						scanf("%d", &codeclient[j]);
+					} 
+					while(codeclient[j]<0 || codeclient[j]>5);
+					//passa controlo para o cliente (kil e depois pause)
+				}
+				write(p1[1], codeclient, sizeof(codeclient));//escreve tentativa na pipe
+				kill(serverpid,SIGCONT);//passa controlo e pausa
+				pause();
+				fflush(stdout);
+				read(p2[0],&posc, sizeof(&posc));
+				read(p2[0],&colorc, sizeof(&colorc));
+				fflush(stdout);
+				if(posc==4)
+				{
+					printf(COLORRESET"Parabens! Voce venceu o jogo do Mastermind!\n");
+					kill(serverpid,SIGTERM);
+					return ;
+				}
+				else
+				{
+					printf(COLORRESET"Tem %d cores em %d posicoes corretas(%d).\n", colorc, posc, (i+1));
+					fflush(stdout);
+				}
+			}
+			printf(RED"Esgotaram-se as tentativas...\n\nGAME OVER\n\n");
+			sleep(1);
+			printf(COLORRESET"");
+			fflush(stdout);
+			kill(serverpid,SIGTERM);
+		}
+		else
+		{
+			//server
+			serverpid=getpid();
+			sleep(1);
+			write(p2[1],&serverpid,sizeof(&serverpid));
+			pause();
+			read(p1[0],&clientpid,sizeof(&clientpid));
+			srand(time(NULL));
+			for(int a=0;a<4;a++)//code choosing
+				codeserver[a]= rand()%6;
 			kill(clientpid,SIGCONT);
 			pause();//passa controlo para o cliente (kil e depois pause)
 			//server checks if code is right
@@ -149,8 +285,6 @@ void jogo()
 							if(colors[codeclient[k]]==0)
 							{
 								rc++;
-								printf("%d bem.\n",rc);
-								fflush(stdout);
 								colors[codeclient[k]]=1;
 							}
 							if(l==k)
@@ -158,8 +292,6 @@ void jogo()
 								if(checkp[k]==0)
 								{
 									rp++;
-									printf("%d no sitio certo.\n",rp);
-									fflush(stdout);
 									checkp[k]=1;
 									break;
 								}
